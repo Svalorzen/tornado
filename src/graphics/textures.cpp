fe40d08a535911fcc64e6534d649750a00d630b9
Svalorzen <svalorzen@gmail.com>
2013-06-07 15:07:23 +0200

    Removed inlining from texture

---
+++ src/graphics/textures.cpp
@@ -0,0 +1,21 @@
+#include "textures.hpp"
+
+#include <unordered_map>
+
+#include <SFML/Graphics.hpp>
+
+namespace Graphics {
+    const sf::Texture & getTexture(std::string textureString) {
+        static std::unordered_map<std::string, sf::Texture> textures_;
+
+        // If we don't have it
+        if ( textures_.find(textureString) == textures_.end() ) {
+            sf::Texture t;
+            if ( ! t.loadFromFile(textureString) ) throw std::runtime_error("Failed to load the requested texture: "+textureString);
+
+            textures_[textureString] = t;
+        }
+
+        return textures_[textureString];
+    }
+}
--- src/graphics/textures.hpp
+++ src/graphics/textures.hpp
@@ -1,26 +1,12 @@
 #ifndef TEXTURE_HEADER_FILE
 #define TEXTURE_HEADER_FILE
 
-#include <SFML/Graphics.hpp>
-
-#include <unordered_map>
 #include <string>
 
+namespace sf { class Texture; }
+
 namespace Graphics {
-    inline const sf::Texture & getTexture(std::string textureString) {
-        static std::unordered_map<std::string, sf::Texture> textures_;
-
-        // If we don't have it
-        if ( textures_.find(textureString) == textures_.end() ) {
-            sf::Texture t;
-            if ( ! t.loadFromFile(textureString) ) throw std::runtime_error("Failed to load the requested texture: "+textureString);
-            
-            textures_[textureString] = t;
-        }
-
-        return textures_[textureString];
-    }
-    
+    const sf::Texture & getTexture(std::string textureString);    
 }
 
 #endif
