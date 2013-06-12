44c205bde394ed97ba5331a42c14db25569b5b71
Svalorzen <svalorzen@gmail.com>
2013-06-12 21:14:56 +0200

    Fixed AnimatedSprite init

--- src/graphics/animated_sprite.cpp
+++ src/graphics/animated_sprite.cpp
@@ -1,8 +1,8 @@
 #include "animated_sprite.hpp"
 
-AnimatedSprite::AnimatedSprite() : Sprite() {}
-AnimatedSprite::AnimatedSprite(const sf::Texture & t) : Sprite(t) {}
-AnimatedSprite::AnimatedSprite(const sf::Texture & t, const sf::IntRect & r) : Sprite(t,r) {}
+AnimatedSprite::AnimatedSprite() : Sprite(), toRender_(true) {}
+AnimatedSprite::AnimatedSprite(const sf::Texture & t) : Sprite(t), toRender_(true) {}
+AnimatedSprite::AnimatedSprite(const sf::Texture & t, const sf::IntRect & r) : Sprite(t,r), toRender_(true) {}
 
 void AnimatedSprite::resize(int width, int height) {
     sf::FloatRect self = Sprite::getLocalBounds();
